19f0819e2dd2e18e5e40ff2629ca5223a84a3744
commanderz <supercommander@ukr.net>
2023-03-01 23:02:25 +0200

    3

--- sw.js
+++ sw.js
@@ -78,41 +78,56 @@ define(['./workbox-148cb7e5'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/_...all_-0855e94e.js",
+    "url": "assets/_...all_-06a9e354.js",
     "revision": null
   }, {
-    "url": "assets/_name_-d10cd280.js",
+    "url": "assets/_name_-87437f5d.js",
     "revision": null
   }, {
-    "url": "assets/404-7c14ca8d.js",
+    "url": "assets/404-9e3b7727.js",
     "revision": null
   }, {
-    "url": "assets/app-3510be3c.js",
+    "url": "assets/app-518ff5ba.js",
     "revision": null
   }, {
-    "url": "assets/default0-ee322f86.js",
+    "url": "assets/default0-e6a08d66.js",
     "revision": null
   }, {
-    "url": "assets/home-07be62c6.js",
+    "url": "assets/home-9dd88209.js",
     "revision": null
   }, {
-    "url": "assets/index-224cafbd.css",
+    "url": "assets/index-2c691b61.css",
     "revision": null
   }, {
-    "url": "assets/virtual_pwa-register-13ed7c5c.js",
+    "url": "assets/start-088d3ab3.js",
+    "revision": null
+  }, {
+    "url": "assets/start-e1cb3cfb.css",
+    "revision": null
+  }, {
+    "url": "assets/TheFooter-145b0695.css",
+    "revision": null
+  }, {
+    "url": "assets/TheFooter-91aad8a8.js",
+    "revision": null
+  }, {
+    "url": "assets/virtual_pwa-register-7871b43c.js",
     "revision": null
   }, {
     "url": "assets/workbox-window.prod.es5-295a6886.js",
     "revision": null
   }, {
     "url": "index.html",
-    "revision": "cae0527b0d9c79eea58346d1aeca82d4"
+    "revision": "85941e4c1b810ae98fee9c2df4f9f646"
+  }, {
+    "url": "start.html",
+    "revision": "a1d336047f49846e8f8e8d50ca689852"
   }, {
     "url": "favicon.svg",
     "revision": "9c0135317804268933c342fb51058593"
   }, {
     "url": "safari-pinned-tab.svg",
-    "revision": "f0cb6c779edb85fd5a81a319885f3310"
+    "revision": "3d0337ab1eb7a73219a025394a203870"
   }, {
     "url": "pwa-192x192.png",
     "revision": "a6e9887c7db720319cc89995f37c5bb4"
